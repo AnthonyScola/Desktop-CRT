9227400b9e142eef853aebc406e1e6089325011d
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-09 15:47:54 +0100

    Setup code for getting screen info

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pygame
 import sys
+
+pygame.init()
+
+# Get Display Size
+screen_info = pygame.display.Info()
+screen_width, screen_height = screen_info.current_w, screen_info.current_h
+
+# Setup full screen display
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME)
